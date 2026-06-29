620abd99f587de914f36b9118a07ae14eb4b2e4c
Harrison Tran <harrisontran357@gmail.com>
2026-06-29 15:03:36 -0700

    navbar fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,8 @@ export default function App() {
     <div className="flex flex-col h-screen w-full bg-gradient-to-br from-stone-200 to-stone-400 overflow-hidden font-sans relative">
       {currentView === "home" ? <HomeView /> : <LogsView />}
 
-      <nav className="absolute bottom-0 left-0 right-0 flex flex-row items-center w-[85%] mx-auto h-32 border-t border-black pb-6 z-50">
+      <nav className="absolute bottom-0 left-0 right-0 flex flex-row items-center w-[85%] mx-auto h-32 border-t border-black backdrop-blur-md bg-stone-400/20 pb-6 z-50">
+        {" "}
         <button
           onClick={() => setCurrentView("home")}
           className={`flex-1 flex flex-col items-center justify-center gap-3 h-full transition-opacity ${currentView === "home" ? "opacity-100" : "opacity-40 hover:opacity-70"}`}
@@ -24,9 +25,7 @@ export default function App() {
             Home
           </span>
         </button>
-
         <div className="w-[1px] h-2/5 bg-black"></div>
-
         <button
           onClick={() => setCurrentView("logs")}
           className={`flex-1 flex flex-col items-center justify-center gap-3 h-full transition-opacity ${currentView === "logs" ? "opacity-100" : "opacity-40 hover:opacity-70"}`}
